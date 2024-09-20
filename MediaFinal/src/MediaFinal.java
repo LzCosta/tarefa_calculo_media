b9bfc1347a1c40958ee14144eeee66a68eb206f2
LzCosta <116127270+LzCosta@users.noreply.github.com>
2024-09-20 04:35:52 -0300

    Commit inicial

---
+++ MediaFinal/src/MediaFinal.java
@@ -0,0 +1,11 @@
+public class MediaFinal {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 3;
+        int nota3 = 5;
+        int nota4 = 7;
+        float mediaFinal = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.println("Sua média final é: "+mediaFinal);
+    }
+}
